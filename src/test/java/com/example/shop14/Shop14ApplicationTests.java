944fcf4055d7f5f43037c4aaa5635da20bff4be8
Viktor-Uv <sev-vic@yandex.com>
2023-11-12 00:17:31 +0100

    Initial commit

---
+++ src/test/java/com/example/shop14/Shop14ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shop14;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Shop14ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
